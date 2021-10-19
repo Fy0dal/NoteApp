e1977dca898305f3d365f31be38fd10cd30fe77e
Fy0dal <90230427+Fy0dal@users.noreply.github.com>
2021-10-19 11:34:51 +0700

    коменты

--- NoteAppUI/NoteApp/Project.cs
+++ NoteAppUI/NoteApp/Project.cs
@@ -8,14 +8,18 @@ using Newtonsoft.Json;
 namespace NoteApp
 {
     /// <summary>
-    /// Класс, который хранит словарь всех созданных заметок.
+    /// Класс, который хранит список заметок.
     /// </summary>
     public class Project
     {
 
-
+        /// <summary>
+        /// Список заметок
+        /// </summary>
         private List<Note> _notes = new List<Note>();
-
+        /// <summary>
+        /// Список заметок
+        /// </summary>
         public List<Note> Notes
         {
             get { return _notes; }
