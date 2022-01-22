7c9e8116dde734e79bd797bc8e16538b685d77ed
Fy0dal <90230427+Fy0dal@users.noreply.github.com>
2022-01-22 11:17:48 +0700

    Изменено название юнит тестов

--- NoteApp/Testing/NoteAppUnitTests/Properties/AssemblyInfo.cs
+++ NoteApp/Testing/NoteAppUnitTests/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // Общие сведения об этой сборке предоставляются следующим набором
 // набора атрибутов. Измените значения этих атрибутов для изменения сведений,
 // связанные со сборкой.
-[assembly: AssemblyTitle("UnitTests")]
+[assembly: AssemblyTitle("NoteAppUnitTests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("UnitTests")]
+[assembly: AssemblyProduct("NoteAppUnitTests")]
 [assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
-[assembly: Guid("98d7f49e-8384-43e3-87b8-fd96003106f9")]
+[assembly: Guid("d3eba45f-a198-43ef-b1e2-28a274958517")]
 
 // Сведения о версии сборки состоят из указанных ниже четырех значений:
 //
